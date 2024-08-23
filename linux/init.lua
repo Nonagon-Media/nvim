05a7bbd2215ca295738c1a9d3d186db3578546fd
Troy Thompson <x-tthompson@zoox.com>
2024-08-23 09:12:13 -0700

    Added git changes

--- linux/init.lua
+++ linux/init.lua
@@ -1,2 +1,14 @@
 require("thompson.core")
 require("thompson.lazy")
+
+-- Custom command to make the current file executable
+vim.api.nvim_create_user_command("MakeExecutable", function()
+	local file = vim.fn.expand("%:p")
+	vim.cmd("!chmod +x " .. file)
+	print(file .. " is now executable.")
+end, {})
+
+-- Optional: Key mapping to trigger MakeExecutable with F3
+vim.keymap.set("n", "<leader>mx", function()
+	vim.cmd.MakeExecutable()
+end, { desc = "Make the current file executable" })
--- mac/lua/thompson/plugins/git.lua
+++ mac/lua/thompson/plugins/git.lua
@@ -1,22 +1,43 @@
--- lua/tthompson/plugins/git.lua - Git Configs
+-- lua/thompson/plugins/git.lua
+
 return {
 	"tpope/vim-fugitive",
 	config = function()
-		-- Key mappings for Git Commands
-		local map = vim.api.nvim_set_keymap
+		-- Use 'map' function for consistency and simplicity
+		local map = vim.keymap.set
 		local opts = { noremap = true, silent = true }
 
-		map("n", "<leader>gs", ":G<CR>", opts) -- Git status
-		map("n", "<leader>gc", ":G commit<CR>", opts) -- Git commit
-		map("n", "<leader>gp", ":G push<CR>", opts) -- Git push
-		map("n", "<leader>gl", ":G pull<CR>", opts) -- Git pull
-		map("n", "<leader>gd", ":G diff<CR>", opts) -- Git diff
-		map("n", "<leader>ga", ":Gwrite<CR>", opts) -- Git add
+		-- Git status
+		map("n", "<leader>gs", ":G<CR>", opts)
 
-		map("n", "<leader>gb', ':G branch<CR>", opts) -- Git branch
-		map("n", "<leader>gbc', ':G branch<CR>", opts) -- New branch
-		map("n", "<leader>gco', ':G checkout<CR>", opts) -- Checkout
-		map("n", "<leader>gbd', ':G branch -d<CR>", opts) -- Delete branch
-		map("n", "<leader>gbr', ':G branch -m<CR>", opts) -- Rename branch
+		-- Git commit
+		map("n", "<leader>gc", ":G commit<CR>", opts)
+
+		-- Git push
+		map("n", "<leader>gp", ":G push<CR>", opts)
+
+		-- Git pull
+		map("n", "<leader>gl", ":G pull<CR>", opts)
+
+		-- Git diff
+		map("n", "<leader>gd", ":G diff<CR>", opts)
+
+		-- Git add (stage changes)
+		map("n", "<leader>ga", ":Gwrite<CR>", opts)
+
+		-- List all branches
+		map("n", "<leader>gb", ":G branch<CR>", opts)
+
+		-- Create a new branch
+		map("n", "<leader>gbc", ":G branch ", opts)
+
+		-- Checkout a branch
+		map("n", "<leader>gco", ":G checkout ", opts)
+
+		-- Delete a branch
+		map("n", "<leader>gbd", ":G branch -d ", opts)
+
+		-- Rename the current branch
+		map("n", "<leader>gbr", ":G branch -m ", opts)
 	end,
 }
